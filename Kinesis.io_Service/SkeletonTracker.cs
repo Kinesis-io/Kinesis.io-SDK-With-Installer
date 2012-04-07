7733cd4975fb3f5fd4f5c2f9e2e096dde4940e95
Achal Aggarwal <aggarwal.achal@gmail.com>
2012-04-07 13:10:17 +0530

    Added depth feedback image

--- Kinesis.io_Service/SkeletonTracker.cs
+++ Kinesis.io_Service/SkeletonTracker.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using Microsoft.Kinect;
 using Coding4Fun.Kinect.Wpf;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
 
 namespace Kinesis.io_Service
 {
@@ -15,6 +18,8 @@ namespace Kinesis.io_Service
         bool skeletonDetected = false;
 
         KinesisSwipeGestureRecognizer kSwipeGestureRecognizer;
+
+        String depthImage;
 
         internal void Start()
         {
@@ -30,9 +35,10 @@ namespace Kinesis.io_Service
 
                 //Initialize to do Skeletal Tracking
                 sensor.ColorStream.Enable();
-                sensor.DepthStream.Enable();
+                sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
                 sensor.SkeletonStream.Enable(parameters);
 
+                sensor.DepthFrameReady += new EventHandler<DepthImageFrameReadyEventArgs>(sensor_DepthFrameReady);
                 sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(sensor_SkeletonFrameReady);
                 sensor.Start();
 
@@ -41,6 +47,121 @@ namespace Kinesis.io_Service
 
                 kSwipeGestureRecognizer = new KinesisSwipeGestureRecognizer();
             }
+        }
+
+        void sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame == null)
+                {
+                    return;
+                }
+
+                //generateDataForWeb(depthFrame);
+                byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                //number of bytes per row width * 4 (B,G,R,Empty)
+                int stride = depthFrame.Width * 4;
+
+                //create image
+                BitmapSource src = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
+                    96, 96, PixelFormats.Bgr32, null, pixels, stride);
+                depthImage = BitmapSourceToBase64(src);
+            }
+        }
+
+        public String BitmapSourceToBase64(BitmapSource bitmap)
+        {
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            BitmapFrame frame = BitmapFrame.Create(bitmap);
+            encoder.Frames.Add(frame);
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                encoder.Save(stream);
+                return Convert.ToBase64String(stream.ToArray());
+            }
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+
+            //get the raw data from kinect with the depth for every pixel
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData);
+
+            //use depthFrame to create the image to display on-screen
+            //depthFrame contains color information for all pixels in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            //Bgr32  - Blue, Green, Red, empty byte
+            //Bgra32 - Blue, Green, Red, transparency 
+            //You must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
+
+            //hardcoded locations to Blue, Green, Red (BGR) index positions       
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+
+            //loop through all distances
+            //pick a RGB color based on distance
+            for (int depthIndex = 0, colorIndex = 0;
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+                depthIndex++, colorIndex += 4)
+            {
+                //get the player (requires skeleton tracking enabled for values)
+                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+
+                //gets the depth value
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+                byte intensity = CalculateIntensityFromDepth(depth);
+
+                //.9M or 2.95'
+                if (depth <= 900)
+                {
+                    //we are very close
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+
+                }
+                // .9M - 2M or 2.95' - 6.56'
+                else if (depth > 900 && depth < 2000)
+                {
+                    //we are a bit further away
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                // 2M+ or 6.56'+
+                else if (depth > 2000)
+                {
+                    //we are the farthest
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+
+                //Color all players "gold"
+                if (player > 0)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = intensity;
+                    pixels[colorIndex + RedIndex] = 0;
+
+                }
+            }
+            return pixels;
+        }
+
+        public static byte CalculateIntensityFromDepth(int distance)
+        {
+            //formula for calculating monochrome intensity for histogram
+            return (byte)(255 - (255 * Math.Max(distance - 100, 0)
+                / (800)));
         }
 
         void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
@@ -96,7 +217,7 @@ namespace Kinesis.io_Service
                     elbow = ElbowLeft;
                 }
 
-                Kinesis.gKinesis.server.SendToAll(String.Format("{{\"cursor\":{{\"x\":{0},\"y\":{1},\"z\":{2}}}}}", scaledCursor.Position.X, scaledCursor.Position.Y, cursor.Position.Z - Spine.Position.Z));
+                Kinesis.gKinesis.server.SendToAll(String.Format("{{\"depthImage\":\"{0}\",\"cursor\":{{\"x\":{1},\"y\":{2},\"z\":{3}}}}}", depthImage, scaledCursor.Position.X, scaledCursor.Position.Y, cursor.Position.Z - Spine.Position.Z));
 
                 kSwipeGestureRecognizer.cursor = cursor;
 
